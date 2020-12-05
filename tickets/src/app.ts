f9216f153df65cc8f1e5b392f3e25ac85f55acc2
Md Ashraf Hussain <ashrafhussain.2317.csedu@gmail.com>
2020-12-05 07:58:04 +0600

    routes added in tickets service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@sgtickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,7 +15,9 @@ app.use(
   })
 );
 
-app.all('*', async (req, res) => {
+app.use(createTicketRouter);
+
+app.all('*', async (req: Request, res: Response) => {
   throw new NotFoundError();
 });
 
